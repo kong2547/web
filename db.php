f9b94fda632f929141842b95ba72d6845cbda4c3
suphawit <kongsupawish@gmail.com>
2025-09-05 13:21:25 +0700

    แก้ไข db.php

--- db.php
+++ db.php
@@ -4,6 +4,28 @@ $db = "n";
 $user = "root";
 $pass = "";
 
+/*CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `user_id` int(6) UNSIGNED DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `action` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `users` (
+  `id` int(6) UNSIGNED NOT NULL,git add .
+
+  `email` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `role` enum('admin','user') DEFAULT 'user',
+  `status` enum('pending','active','rejected') DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+ */
+
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
